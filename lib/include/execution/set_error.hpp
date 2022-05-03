63567cdc43138a00476a3bec82731bc22543017a
sharpeye <misko.pawel@gmail.com>
2022-05-03 19:39:54 +0300

    set_error as CPO

--- lib/include/execution/set_error.hpp
+++ lib/include/execution/set_error.hpp
@@ -1,15 +1,33 @@
 #pragma once
 
+#include "customization.hpp"
+
 #include <utility>
 
 namespace execution {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-template <typename R, typename E>
-constexpr void set_error(R&& receiver, E&& error)
+inline constexpr struct set_error_fn
 {
-    std::forward<R>(receiver).set_error(std::forward<E>(error));
-}
+    // default implementation
+    template <typename R, typename E>
+        requires (!is_tag_invocable_v<set_error_fn, R&&, E&&>)
+    void operator () (R&& receiver, E&& error) const
+    {
+        std::forward<R>(receiver).set_error(std::forward<E>(error));
+    }
+
+    template <typename R, typename E>
+        requires is_tag_invocable_v<set_error_fn, R&&, E&&>
+    void operator () (R&& receiver, E&& error) const
+    {
+        return execution::tag_invoke(
+            *this,
+            std::forward<R>(receiver),
+            std::forward<E>(error));
+    }
+
+} set_error;
 
 }   // namespace execution
