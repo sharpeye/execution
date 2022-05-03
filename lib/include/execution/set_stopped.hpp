d5c427fe7283407fe1ea75dc63574038d5c9526e
sharpeye <misko.pawel@gmail.com>
2022-05-03 19:37:07 +0300

    set_stopped as CPO

--- lib/include/execution/set_stopped.hpp
+++ lib/include/execution/set_stopped.hpp
@@ -1,15 +1,30 @@
 #pragma once
 
+#include "customization.hpp"
+
 #include <utility>
 
 namespace execution {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-template <typename R>
-constexpr void set_stopped(R&& receiver)
+inline constexpr struct set_stopped_fn
 {
-    std::forward<R>(receiver).set_stopped();
-}
+    // default implementation
+    template <typename R>
+        requires (!is_tag_invocable_v<set_stopped_fn, R&&>)
+    void operator () (R&& receiver) const
+    {
+        std::forward<R>(receiver).set_stopped();
+    }
+
+    template <typename R>
+        requires is_tag_invocable_v<set_stopped_fn, R&&>
+    void operator () (R&& receiver) const
+    {
+        return execution::tag_invoke(*this, std::forward<R>(receiver));
+    }
+
+} set_stopped;
 
 }   // namespace execution
